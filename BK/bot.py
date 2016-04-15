dd505cb7d6aaa92defb148274d28a352e5d4ba0f
kyank <160918@supinfo.com>
2016-04-15 15:52:25 +0200

    edit bot interaction

--- BK/bot.py
+++ BK/bot.py
@@ -10,15 +10,22 @@ API_TOKEN = os.environ['API_TOKEN']
 bot = telebot.TeleBot(API_TOKEN)
 
 
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start','freebk'])
+@bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, roburger.burgermain())
+    bot.reply_to(message, """ Hi !
+    This bot generate a code for some free burgers !
+    Type /freebk, *wait 10 sec* while your code is generated and Bon appetit ! """)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['freebk'])
+def send_code(message):
+    bot.reply_to(message, roburger.burgermain() + "/n-  Bon appetit!")
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    bot.reply_to(message, message.text)
+    bot.reply_to(message, """Type /freebk, get burgers!""")
 
 bot.polling()
