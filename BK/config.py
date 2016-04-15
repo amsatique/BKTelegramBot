4b3bfec7c860592f40ffaa7dff37e0d3c2b7ac64
kyank <160918@supinfo.com>
2016-04-15 16:04:09 +0200

    edit bot interaction

--- BK/config.py
+++ BK/config.py
@@ -2,20 +2,21 @@
 url = 'https://www.bkvousecoute.fr'
 
 # Restaurant ID
-ID = '20771' # Boetie restaurant
+ID = '20771'  # Boetie restaurant
 
 # Input here date and time:
-date = ['02','15']
-time = ['12','30']
+date = ['02', '15']
+time = ['12', '30']
 
 # HTTP inputs:
 headers = {
-        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Encoding':'gzip, deflate, sdch',
-        'Accept-Language':'en-US,en;q=0.8,fr;q=0.6,q=0.4',
-        'Cache-Control':'no-cache',
-        'Connection':'keep-alive',
-        'Host':'www.bkvousecoute.fr',
-        'Pragma':'no-cache',
-        "User-Agent" : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36'
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'en-US,en;q=0.8,fr;q=0.6,q=0.4',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'Host': 'www.bkvousecoute.fr',
+        'Pragma': 'no-cache',
+        "User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40' +
+                      '.0.2214.115 Safari/537.36'
         }
