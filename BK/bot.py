6af6b87dc890c3ba5f1fce34945cb32f73173c9c
300KN <kv.kach2@gmail.com>
2016-04-24 23:56:14 +0200

    update UI/UX Count orders done

--- BK/bot.py
+++ BK/bot.py
@@ -1,26 +1,41 @@
-# This is a simple echo bot using the decorator mechanism.
-# It echoes any incoming text messages.
-
 import telebot
 import roburger
 import os
 
-API_TOKEN = os.environ['API_TOKEN']
+# Emojis and cute stuff
+hourglass = u'\U0000231B'
+hamburger = u'\U0001F354'
+okhandsign = u'\U0001F44C'
+star = u'\U00002B50'
+thumbsupsign = u'\U0001F44D'
+clappinghandsign = u'\U0001F44D'
 
+
+API_TOKEN = os.environ['API_TOKEN']
 bot = telebot.TeleBot(API_TOKEN)
 
 
+# Handle '/start and /help'
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, """ Hi !
-    This bot generate a code for some free burgers !
-    Type /freebk, *wait 10 sec* while your code is generated and Bon appetit ! """)
+    bot.reply_to(message, """ Hi ! This bot generate a code for some free """ + hamburger + """ """ + hamburger + """!
+# - Type /freebk
+# - """+hourglass+""" for 10 sec """+hourglass+"""
+# - Bon appetit ! """)
 
 
-# Handle '/start' and '/help'
+# Handle '/freebk', our main stuff
 @bot.message_handler(commands=['freebk'])
 def send_code(message):
-    bot.reply_to(message, roburger.burgermain() + ". Bon appetit!")
+    bot.reply_to(message, roburger.burgermain() + "\n    "+hamburger+"    Bon appetit!   " + hamburger)
+
+
+# Handle '/about', giving people informations
+@bot.message_handler(commands=['about'])
+def send_welcome(message):
+    burgernumber = roburger.generated_burger()
+    bot.reply_to(message, """Over """ + str(burgernumber) + ' ' + hamburger + """ has been generated ! """+thumbsupsign+""" If you like our bot, please put """ + star + star + star + star + star + """ here :
+http://telegram.me/storebot?start=bkcodebot""")
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
--- BK/roburger.py
+++ BK/roburger.py
@@ -1,12 +1,13 @@
 import random
 import ssl
 from collections import defaultdict
+import pickle
 
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.poolmanager import PoolManager
 from robobrowser import RoboBrowser
-
 import config
+
 
 def max_radio_map(brow):
     radio_map = defaultdict(list)
@@ -35,13 +36,23 @@ class BKBrowser(object):
     def __init__(self):
         # Browse url :
         self.result = None
+        self.burger_count = 0
         self.browser = RoboBrowser(parser="html.parser")
         self.browser.session.headers = config.headers
         # Mount with custom SSL Adapter
         self.browser.session.mount('https://', HTTPSAdapter())
 
+    def _saving_count(self):
+        with open('burgercount.txt', 'w') as f:
+            f.write(str(self.burger_count))
+
+    def _load_count(self):
+        with open('burgercount.txt') as f:
+            self.burger_count = f.read()
+
     def _connect(self):
         # Get to website
+        self._load_count()
         print("- Connecting to url ...")
         self.browser.open(config.url)
 
@@ -74,16 +85,28 @@ class BKBrowser(object):
         form.serialize()
         self.browser.submit_form(form)
 
+    def _add_burger_count(self):
+        print(self.burger_count)
+        self.burger_count = int(self.burger_count) + 1
+        print(self.burger_count)
+        self._saving_count()
+
     def get_validation_code(self):
         self._connect()
         self._skip_first_page()
         self._fill_date_form()
         self._fill_form()
         self.result = self.browser.find('p', {'class': 'ValCode'}).text
+        self._add_burger_count()
         return self.result
 
     def return_result(self):
         return self.result
+
+
+def generated_burger():
+    with open('burgercount.txt') as f:
+        return f.read()
 
 
 def burgermain():
